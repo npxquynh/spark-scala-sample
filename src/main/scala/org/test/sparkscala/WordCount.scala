8f0da16c0f42f20ba7b5ec1ca67a65c62f63523c
Quynh Nguyen <quynh.xq@gmail.com>
2016-07-18 15:39:20 +0200

    WordCount simple program

---
+++ src/main/scala/org/test/sparkscala/WordCount.scala
@@ -0,0 +1,23 @@
+package org.test.sparkscala
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object WordCount {
+  def main(args: Array[String]) = {
+    val conf = new SparkConf()
+      .setAppName("WordCount")
+      .setMaster("local")
+    val sc = new SparkContext(conf)
+    
+    val test = sc.textFile("./data/food.txt")
+    
+    test.flatMap { line =>
+      line.split(" ")
+    }
+    .map { word =>
+      (word, 1)
+    }
+    .saveAsTextFile("./output/foodcount")
+  }
+}
